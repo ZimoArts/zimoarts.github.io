c9084346bf9e65436a6f8e2cdcc8fa4116cc607b
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-04 00:50:31 +0800

    add header and footer

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-full h-36 flex justify-start border-b border-black">
+        <div class="relative w-full mb-2">
+            <div class="absolute bottom-0 left-9 text-lg">ZIMOART</div>
+            <div class="absolute bottom-0 right-9 text-2xl flex justify-end items-center">
+                <div class="pr-6 h-8">SCULPTURE</div>
+                <div class="w-1.5 h-6 bg-black mr-6"></div>
+                <div class="pr-6 h-8">PHOTOGRAPHIC WORKS</div>
+                <div class="w-1.5 h-6 bg-black mr-6"></div>
+                <div class="pr-6 h-8">DRAMATIC WORK</div>
+                <div class="w-1.5 h-6 bg-black mr-6"></div>
+                <div class="pr-6 h-8">OTHER</div>
+            </div>
+        </div>
+    </div>
+</template>
+
