da7c90b31c156748588a988572ed9facfefa51c5
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-04 00:12:07 +0800

    add home view structure

---
+++ src/components/BottomView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-full h-24 flex justify-center mb-9 relative">
+        <div class="absolute bottom-0 left-9 text-xs">
+            2022 GROTON SCHOOL.ALL RIGHTS RESERVED.
+        </div>
+        <div class="absolute bottom-0 right-9 text-right">
+            <div class="text-lg">ZIMOART</div>
+            <div class="text-xs">JGYTDETRTQWEESDFG</div>
+            <div class="text-xs">GDGHGHGDFGSDTSIUHIOVBBXZMIYTUXDG</div>
+            <div class="text-xs">HJHFJH JHCH V FJ VJVCCCC H</div>
+            <div class="text-xs">GJHG JKHVJHG VHVHFV</div>
+            <div class="text-xs">CHGFD</div>
+        </div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,73 @@
 <template>
-    this is home view
+    <div class="w-full">
+        <div class="w-full flex justify-center">
+            <div class="w-2/5">
+
+            </div>
+            <div class="w-3/5 bg-white">
+                <div class="mt-20 mr-9">
+
+                    <div class="w-full flex h-36 relative">
+                        <div class="w-full absolute bottom-0 flex mr-9 border-b border-black">
+                            <div class="h-28 w-56 bg-black">
+                            </div>
+                            <div class="w-full h-28 relative">
+                                <div class="absolute bottom-0 right-0">
+                                    <div class="text-lg">SCULPTURE</div>
+                                    <div class="h-2 bg-black"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="w-full flex h-36 relative">
+                        <div class="w-4/5 absolute bottom-0 right-0 flex border-b border-black">
+                            <div class="h-28 w-56 bg-black">
+                            </div>
+                            <div class="w-full h-28 relative">
+                                <div class="absolute bottom-0 right-0">
+                                    <div class="text-lg">PHOTOGRAPHIC WORKS</div>
+                                    <div class="h-2 bg-black"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="w-full flex h-36 relative">
+                        <div class="w-full absolute bottom-0 flex border-b border-black">
+                            <div class="h-28 w-56 bg-black">
+                            </div>
+                            <div class="w-full h-28 relative">
+                                <div class="absolute bottom-0 right-0">
+                                    <div class="text-lg">DRAMATIC WORK</div>
+                                    <div class="h-2 bg-black"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="w-full flex h-36 relative">
+                        <div class="w-4/5 absolute bottom-0 right-0 flex border-b border-black">
+                            <div class="h-28 w-56 bg-black">
+                            </div>
+                            <div class="w-full h-28 relative">
+                                <div class="absolute bottom-0 right-0">
+                                    <div class="text-lg">OTHER</div>
+                                    <div class="h-2 bg-black"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <BottomView class="mt-24" />
+    </div>
 </template>
 
 <script setup>
-import HelloWorld from '../components/HelloWorld.vue'
+
+import BottomView from '../components/BottomView.vue'
 </script>
 
