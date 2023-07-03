7ea13c45e14625f7110e1e9f2f0567bea0b0f718
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-04 00:50:47 +0800

    add sculpture view

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SculptureView from '../views/SculptureView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/sculpture',
+            name: 'sculpture',
+            component: SculptureView
+        }
+    ]
 })
 
 export default router
---
+++ src/views/SculptureView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="w-full">
+        <Header />
+        This is Sculpture View
+        
+    </div>
+</template>
+
+<script setup>
+import Header from '../components/Header.vue'
+</script>
