0f64d2d13a98eaac4109c3b0df068dde87f91a07
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-04 22:03:28 +0800

    feat: add daisy UI

---
+++ src/components/HorizontalImageListView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-full">
+        <div class="carousel rounded-box">
+            <div class="carousel-item">
+                <img src="/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Burger" />
+            </div>
+            <div class="carousel-item">
+                <img src="/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" />
+            </div>
+            <div class="carousel-item">
+                <img src="/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Burger" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+export default {
+
+}
+</script>
