c65eb8a8ee126933cfb79a3d0b88912efbcec4eb
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-10 22:38:07 +0800

    feat: link to home page

--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="w-full h-44 flex justify-start border-b border-black">
         <div class="relative w-full mb-2">
-            <div class="absolute bottom-0 left-9 text-lg">
+            <router-link to="/" class="absolute bottom-0 left-9 text-lg hover:cursor-pointer">
                 <img class="w-96" src="../assets/header_icon.png" alt="">
-            </div>
+            </router-link>
             <div class="absolute bottom-0 right-9 text-2xl flex justify-end items-center">
                 <router-link to="/sculpture" class="pr-6 h-8 hover:cursor-pointer">SCULPTURE</router-link>
                 <div class="w-1.5 h-6 bg-black mr-6"></div>
