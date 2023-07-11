4a5e005317ee9680e62b07370377227e238c73e1
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-11 22:06:12 +0800

    feat: add image preview component

--- src/components/HorizontalImageListView.vue
+++ src/components/HorizontalImageListView.vue
@@ -1,31 +1,53 @@
 <template>
     <div class="w-full">
-        <div class="carousel rounded-box">
-            <template v-for="image in imageList">
-                <div class="carousel-item bg-white mr-6">
-                    <img :src="image.Url" alt="" />
+        <div class="w-full carousel rounded-box">
+            <template v-for="info in infoList">
+                <div class="carousel-item bg-white mr-6" @click="handleTapImage(info.index)">
+                    <div class="w-96">
+                        <img class="w-full" :src="info.url" alt="" />
+                        <div class="w-full mt-9">
+                            <div class="w-64 text-sm font-bold">{{ info.title }}</div>
+                            <div class="w-64 text-sm">{{ info.subTitle }}</div>
+                        </div>
+                    </div>
                 </div>
             </template>
         </div>
     </div>
+    <fox-preview-image v-model="showPreview" :initial-index="previewIndex" :src="previewImageSrc" z-index="9000"></fox-preview-image>
 </template>
 
 <script setup>
 
-import { ref } from 'vue'
+import { onMounted, reactive, ref } from 'vue'
 
 const props = defineProps({
-    images: Array
+    infos: Array
 })
-let imageList = ref([])
-imageList = props.images
 
-function AddImages(images) {
-    imageList.push(images);
+const imagePrevewConfig = reactive({
+    "urlList": []
+})
+
+const infoList = ref([])
+const showPreview = ref(false)
+const previewImageSrc = ref('')
+const previewIndex = ref(0)
+
+
+onMounted(() => {
+    infoList.value = props.infos
+    for (let index = 0; index < infoList.value.length; index++) {
+        infoList.value[index]['index'] = index
+        imagePrevewConfig.urlList.push(infoList.value[index]['url'])
+    }
+    previewImageSrc.value = imagePrevewConfig.urlList
+})
+
+
+function handleTapImage(index) {
+    previewIndex.value = index
+    showPreview.value = true
 }
-
-defineExpose({
-    AddImages
-})
 
 </script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import foxPreviewImage from "fox-preview-image";
+import "fox-preview-image/lib/style.css";
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(foxPreviewImage)
 app.mount('#app')
