30ffec1f8a879e15b111238a487fa4b58c7828f7
chenjunqian <chenjunqian0810@foxmail.com>
2023-07-30 23:55:58 +0800

    feat: set if has title image

--- src/components/ContentDetailView.vue
+++ src/components/ContentDetailView.vue
@@ -6,7 +6,8 @@
             <div class="flex justify-start mt-28">
                 <div class="w-3/5">
                     <template v-for="mainImage in item.mainImageList">
-                        <img @click="handleTapImage(mainImage)" class="w-full mb-4 object-contain hover:cursor-pointer" :src="mainImage" alt="">
+                        <img @click="handleTapImage(mainImage)" class="w-full mb-4 object-contain hover:cursor-pointer"
+                            :src="mainImage" alt="">
                     </template>
                 </div>
                 <div class="w-2/5 ml-6">
@@ -15,7 +16,8 @@
                         <div class="absolute bottom-4 right-0">
                             <div class="flex justify-end mt-9">
                                 <template v-for="image in item.imageList">
-                                    <img @click="handleTapImage(image)" class="w-3/4 bg-gray-50 object-contain hover:cursor-pointer" :src="image" />
+                                    <img @click="handleTapImage(image)"
+                                        class="w-3/4 bg-gray-50 object-contain hover:cursor-pointer" :src="image" />
                                 </template>
                             </div>
                         </div>
@@ -28,27 +30,66 @@
             </div>
         </template>
         <template v-if="imageListCount == 2">
-            <div class="w-64 text-lg font-bold">{{ item.title }}</div>
-            <div class="w-56">{{ item.subTitle }}</div>
-            <div class="flex justify-start mt-28">
-                <div class="w-3/5">
-                    <template v-for="mainImage in item.mainImageList">
-                        <img @click="handleTapImage(mainImage)" class="w-full mb-4 object-contain hover:cursor-pointer" :src="mainImage" alt="">
-                    </template>
-                </div>
-                <div class="w-2/5 ml-6">
-                    <div class="relative h-full">
-                        <div class="">{{ item.description }}</div>
-                        <div class="absolute bottom-4 right-0">
-                            <div class="flex justify-end mt-9">
-                                <template v-for="image in item.imageList">
-                                    <img @click="handleTapImage(image)" class="w-64 ml-3 object-contain hover:cursor-pointer" :src="image" />
-                                </template>
+            <template v-if="hasTitleImageImage">
+                <div class="flex">
+                    <div class="w-2/5">
+                        <template v-for="mainImage in item.mainImageList">
+                            <img @click="handleTapImage(mainImage)" class="w-full object-contain hover:cursor-pointer"
+                                :src="mainImage" alt="">
+                        </template>
+                    </div>
+                    <div class="w-3/5 relative">
+                        <div class="w-full flex">
+                            <div class="w-2/3 flex justify-start">
+                                <div class="w-full ml-9">
+                                    <div class="w-64 text-lg font-bold">{{ item.title }}</div>
+                                    <div class="w-56">{{ item.subTitle }}</div>
+                                </div>
+                            </div>
+                            <div class="w-1/3 flex justify-end">
+                                <img @click="handleTapImage(mainImage)"
+                                    class="w-64 mb-4 object-contain hover:cursor-pointer" :src="item.titleImage" alt="">
+                            </div>
+                        </div>
+                        <div class="mt-9 ml-9">
+                            <div class="">{{ item.description }}</div>
+                            <div class="absolute bottom-0 right-0">
+                                <div class="flex justify-end mt-9">
+                                    <template v-for="image in item.imageList">
+                                        <img @click="handleTapImage(image)"
+                                            class="w-64 ml-3 object-contain hover:cursor-pointer" :src="image" />
+                                    </template>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </template>
+            <template v-else>
+                <div class="w-64 text-lg font-bold">{{ item.title }}</div>
+                <div class="w-56">{{ item.subTitle }}</div>
+                <div class="flex justify-start mt-28">
+                    <div class="w-3/5">
+                        <template v-for="mainImage in item.mainImageList">
+                            <img @click="handleTapImage(mainImage)" class="w-full mb-4 object-contain hover:cursor-pointer"
+                                :src="mainImage" alt="">
+                        </template>
+                    </div>
+                    <div class="w-2/5 ml-6">
+                        <div class="relative h-full">
+                            <div class="">{{ item.description }}</div>
+                            <div class="absolute bottom-4 right-0">
+                                <div class="flex justify-end mt-9">
+                                    <template v-for="image in item.imageList">
+                                        <img @click="handleTapImage(image)"
+                                            class="w-64 ml-3 object-contain hover:cursor-pointer" :src="image" />
+                                    </template>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </template>
             <div class="mt-9">
                 <div class="w-64 text-sm font-bold">{{ item.bottomTitle }}</div>
                 <div class="w-64 text-sm">{{ item.bottomSubTitle }}</div>
@@ -60,7 +101,8 @@
             <div class="flex justify-start mt-28">
                 <div class="w-2/5">
                     <template v-for="mainImage in item.mainImageList">
-                        <img @click="handleTapImage(mainImage)" class="w-full mb-4 object-contain hover:cursor-pointer" :src="mainImage" alt="">
+                        <img @click="handleTapImage(mainImage)" class="w-full mb-4 object-contain hover:cursor-pointer"
+                            :src="mainImage" alt="">
                     </template>
                 </div>
                 <div class="w-3/5 ml-6">
@@ -69,7 +111,8 @@
                         <div class="absolute bottom-4 right-0">
                             <div class="flex justify-end">
                                 <template v-for="image in item.imageList">
-                                    <img @click="handleTapImage(image)" class="w-64 ml-3 object-contain hover:cursor-pointer" :src="image" />
+                                    <img @click="handleTapImage(image)"
+                                        class="w-64 ml-3 object-contain hover:cursor-pointer" :src="image" />
                                 </template>
                             </div>
                         </div>
@@ -99,6 +142,11 @@ let item = ref({})
 let imageListCount = ref(0)
 item = props.itemDetail
 imageListCount = item.imageList.length
+
+const hasTitleImageImage = ref(false)
+if (item.titleImage != "") {
+    hasTitleImageImage.value = true
+}
 
 function handleTapImage(url) {
     v3ImgPreviewFn(url)
