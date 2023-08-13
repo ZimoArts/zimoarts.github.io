4344c509a98667d00072dfdef6ce0e286b64ce8b
chenjunqian <chenjunqian0810@foxmail.com>
2023-08-13 23:33:25 +0800

    feat: add splash scoll image

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VImagesPreview from 'v3-img-preview'
+import Carousel from "v3-carousel";
 
 const app = createApp(App)
 
 app.use(router)
 app.use(VImagesPreview)
+app.use(Carousel)
 app.mount('#app')
