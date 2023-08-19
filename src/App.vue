67de8090c0b8d94da29a3f3e6333e724d4526191
chenjunqian <chenjunqian0810@foxmail.com>
2023-08-19 22:24:49 +0800

    style: set image preview backgroup color

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,11 @@ import { RouterView } from 'vue-router'
 <template>
     <RouterView />
 </template>
+
+<style lang="less">
+#v3-img-preview-container-id {
+    .v-images-wrap {
+        background: rgba(0, 0, 0, 0.8);
+    }
+}
+</style>
