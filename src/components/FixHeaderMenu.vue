231240c5a954dbb18afc4543d902e0d4fafb9647
chenjunqian <chenjunqian0810@foxmail.com>
2023-10-30 01:22:27 +0800

    update style

--- src/components/FixHeaderMenu.vue
+++ src/components/FixHeaderMenu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full flex justify-end border-b border-black bg-white pr-9 z-50">
+    <div class="w-full flex justify-end border-b border-black bg-white z-50">
         <div class="text-lg flex justify-end items-center">
             <router-link to="/sculpture" class="pr-6 h-8 hover:cursor-pointer">SCULPTURE</router-link>
             <div class="w-1.5 h-6 bg-black mr-6"></div>
@@ -9,7 +9,7 @@
             <div class="w-1.5 h-6 bg-black mr-6"></div>
             <router-link to="/interview" class="pr-6 h-8 hover:cursor-pointer">INTERVIEW</router-link>
             <div class="w-1.5 h-6 bg-black mr-6"></div>
-            <router-link to="/other" class="pr-6 h-8 hover:cursor-pointer">DIGITAL DESIGN</router-link>
+            <router-link to="/other" class="h-8 hover:cursor-pointer">DIGITAL DESIGN</router-link>
         </div>
     </div>
 </template>
--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -4,7 +4,7 @@
             <router-link to="/" class="absolute -bottom-2 left-9 text-lg hover:cursor-pointer">
                 <img class="w-80 hover:cursor-pointer hover:scale-110 ease-in-out duration-300 delay-150" src="../assets/header_icon.png" alt="">
             </router-link>
-            <div class="absolute bottom-0 right-9 text-lg flex justify-end items-center">
+            <div class="absolute bottom-0 right-0 text-lg flex justify-end items-center">
                 <router-link to="/sculpture" class="pr-6 h-8 hover:cursor-pointer">SCULPTURE</router-link>
                 <div class="w-1.5 h-6 bg-black mr-6"></div>
                 <router-link to="/photography" class="pr-6 h-8 hover:cursor-pointer">PHOTOGRAPHY WORK</router-link>
@@ -13,7 +13,7 @@
                 <div class="w-1.5 h-6 bg-black mr-6"></div>
                 <router-link to="/interview" class="pr-6 h-8 hover:cursor-pointer">INTERVIEW</router-link>
                 <div class="w-1.5 h-6 bg-black mr-6"></div>
-                <router-link to="/other" class="pr-6 h-8 hover:cursor-pointer">DIGITAL DESIGN</router-link>
+                <router-link to="/other" class="h-8 hover:cursor-pointer">DIGITAL DESIGN</router-link>
             </div>
         </div>
     </div>
